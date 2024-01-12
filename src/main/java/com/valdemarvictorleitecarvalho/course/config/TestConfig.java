88a8d08234e222e0b3c06587ded0178c8cd3a1aa
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2024-01-12 14:12:00 -0300

    Payment, one-to-one association

--- src/main/java/com/valdemarvictorleitecarvalho/course/config/TestConfig.java
+++ src/main/java/com/valdemarvictorleitecarvalho/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.valdemarvictorleitecarvalho.course.entities.Category;
 import com.valdemarvictorleitecarvalho.course.entities.Order;
 import com.valdemarvictorleitecarvalho.course.entities.OrderItem;
+import com.valdemarvictorleitecarvalho.course.entities.Payment;
 import com.valdemarvictorleitecarvalho.course.entities.Product;
 import com.valdemarvictorleitecarvalho.course.entities.User;
 import com.valdemarvictorleitecarvalho.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2023-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
